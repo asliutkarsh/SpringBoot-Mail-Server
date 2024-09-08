857f41688c3f6c5c3f34fa6ff4c1d333716cc165
Utkarsh Jaiswal <utkarshjaiswal2580@gmail.com>
2024-09-08 18:37:04 +0530

    Added Cors

---
+++ src/main/java/com/asliutkarsh/springbootmailserver/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.asliutkarsh.springbootmailserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class SecurityConfig {
+
+    private static final String ALLOWED_ORIGINS = List.of("http://localhost:3000").toString();
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(ALLOWED_ORIGINS).allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
